9bc37219428a0d26d29e1c990cc057027566dfc0
Sunnatjon <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-07-25 22:32:03 +0500

    models.py

--- apps/models.py
+++ apps/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 
-
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)  # Added unique constraint
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']  # Order categories by name
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     price = models.FloatField(default=0)
-    category = models.ForeignKey(Category, models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('product_detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['name'] 
