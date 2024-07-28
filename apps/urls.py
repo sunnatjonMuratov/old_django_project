e8a255c5328dd2d5403bc31784a4364941fea2d4
Sunnatjon <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-07-28 19:20:30 +0500

    Update urls.py

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 from apps.views import Main
 from djangoProject.settings import MEDIA_URL, MEDIA_ROOT
@@ -15,10 +16,11 @@ def send_email(request):
 
     return JsonResponse()
 
+app_name = 'myapp'
+
 
 urlpatterns = [
     path('', Main.as_view(), name='index_view')
-
+    path('detail/<int:id>/', views.detail, name='detail'),
 ]
-
 
