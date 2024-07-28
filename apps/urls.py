c70d4956091cd965676352c920038f22188a228f
Sunnatjon <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-07-28 19:12:32 +0500

    urls.py

--- apps/urls.py
+++ apps/urls.py
@@ -5,15 +5,15 @@ from django.urls import path, include
 from apps.views import Main
 from djangoProject.settings import MEDIA_URL, MEDIA_ROOT
 
-# def send_email(request):
-#     email = request.POST['email']
-#     subject = 'django tema'
-#     msg = 'Django grupadan xabar'
-#     start = time()
-#     send_mail(subject, msg, EMAIL_HOST_USER, [email])
-#     end = time()
-#
-#     return JsonResponse()
+def send_email(request):
+    email = request.POST['email']
+    subject = 'django tema'
+    msg = 'Django grupadan xabar'
+    start = time()
+    send_mail(subject, msg, EMAIL_HOST_USER, [email])
+    end = time()
+
+    return JsonResponse()
 
 
 urlpatterns = [
