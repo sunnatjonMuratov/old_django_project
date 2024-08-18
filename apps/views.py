9d653e05a719336433eb0df9f1f75130f34a3215
Sunnatjon Muratov <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-08-18 20:57:19 +0500

    Update views.py

--- apps/views.py
+++ apps/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from apps.models import Product, Category
 
+
 def index_view(request):
     search = request.GET.get('search', '')  # Default to an empty string if no search term
     products = Product.objects.filter(category__name__icontains=search) if search else Product.objects.all()
@@ -16,6 +17,7 @@ def main_view(request):
         'categories': categories
     }
     return render(request, 'apps/main.html', context)
+
 
 class MainView(TemplateView):  # Renamed to avoid conflict with the function-based view
     template_name = 'apps/index.html'
