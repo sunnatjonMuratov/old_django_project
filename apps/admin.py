26afb705022e58b98847bc95bba224b9c189144e
Sunnatjon Muratov <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-08-18 20:56:11 +0500

    Update admin.py

--- apps/admin.py
+++ apps/admin.py
@@ -6,17 +6,8 @@ from apps.models import Product, Category
 class ProductModelAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'price', 'image',
     search_fields = 'name',
-    list_editable = 'price',
-
-    # def __init__(self, model, admin_site):
-    #     super().__init__(model, admin_site)
-    #
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    #
-    # def has_change_permission(self, request, obj=None):
-    #     return False
-
+    list_editable = 'price'
+    
 
 @admin.register(Category)
 class CategoryModelAdmin(admin.ModelAdmin):
